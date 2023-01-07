bc9a395c0b302b25e55ad85f96ef16f9a14bf30d
Mustyatiq <atikumusti@gmail.com>
2023-01-07 09:20:12 -0800

    new

---
+++ 0x00-hello_world/101.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * Return - 1
+ **/
+
+int main(void)
+{
+	echo -e "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+		return (1);
+}
