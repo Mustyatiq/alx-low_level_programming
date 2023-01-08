035e867e94e4d035eed0770180d11eb31bcbe0f7
Mustyatiq <atikumusti@gmail.com>
2023-01-08 11:20:03 -0800

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,27 +11,32 @@ int main(void)
 	int p = n + 1;
 	int q = p + 1;
 
-	while (n <  57)
+	while (n <  56)
 	{
-		putchar(n);
-		putchar(p);
-		putchar(q);
-
-		if (n == 56 && p == 57)
-			break;
-
-		putchar(44);
-		putchar(32);
-		q++;
-
-		if (q == 58)
+		while (p < 57)
 		{
-			p++;
-			q = p + 1;
-		} else if (p == 57 && q == 58)
-			n++;
-	}
+			putchar(n);
+			putchar(p);
+			putchar(q);
 
+			if (n == 55 && p == 56 && q == 57)
+				break;
+
+			putchar(44);
+			putchar(32);
+			q++;
+
+			if (q == 58)
+			{
+				p++;
+				q = p + 1;
+			}
+		}
+
+		n++;
+		p = n + 1;
+		q = p + 1;
+	}
 		putchar('\n');
 		return (0);
 }
