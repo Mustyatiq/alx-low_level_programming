1692c7f6aba3db2a2875d7624ee9033602887c40
Mustyatiq <atikumusti@gmail.com>
2023-01-08 06:36:05 -0800

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 - 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+}
