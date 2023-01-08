ac7ac89cdae4acb0eafe4dc384c92b74257c4ac6
Mustyatiq <atikumusti@gmail.com>
2023-01-08 08:52:35 -0800

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the possible combinations of two digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+	int p = n + 1;
+
+	while (n < 57)
+	{
+		putchar(n);
+		putchar(p);
+
+		if (n == 56 && p == 57)
+			goto s;
+		putchar(44);
+		putchar(32);
+s:
+		p++;
+		if (p == 57)
+		{
+			n++;
+			p = n + 1;
+		}
+	}
+}
+
