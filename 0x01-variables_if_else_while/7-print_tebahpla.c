b39105fceb6f8daeb69cbcad153018dcdd82f57c
Mustyatiq <atikumusti@gmail.com>
2023-01-08 06:43:13 -0800

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints alphabet in lowercase and in reverse
+ *   * Return: 0
+ *    */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n > 96)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+
+	return (0);
+}
