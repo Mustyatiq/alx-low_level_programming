70aae5335df463763783443a5eecfe6d881c3e29
Mustyatiq <atikumusti@gmail.com>
2023-01-09 05:48:17 -0800

    new

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of two two-digit number
+ * Return: 0
+*/
+int main(void)
+{
+	int a = 48;
+	int b = 48;
+	int c = a;
+	int d = b + 1;
+
+	while (a < 58)
+	{
+		while (c < 58)
+		{
+			if (a == 57 && b == 57)
+				break;
+			putchar(a);
+			putchar(b);
+			putchar(32);
+			putchar(c);
+			putchar(d);
+			if (b == 56 && c == 57 && d == 57)
+				goto s;
+			putchar(44);
+			putchar(32);
+s:
+			d++;
+			if (d > 57)
+			{
+				c++;
+				d = 48;
+			}
+		}
+		b++;
+		if (b == 58)
+		{
+			a++;
+			b = 48;
+		}
+		c = a;
+		if (b == 57)
+			d = b;
+		else
+			d = b + 1;
+	}
+	putchar('\n');
+	return (0);
+}
