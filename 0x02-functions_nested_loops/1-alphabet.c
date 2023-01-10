f07ce52fe603b0c176d0a8d1d347d87af3c77854
Mustyatiq <atikumusti@gmail.com>
2023-01-10 06:06:20 -0800

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,7 +14,7 @@ int main(void)
 
 void print_alphabet(void)
 {
-	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
 	_putchar(alpha);
 	_putchar('\n');
 
