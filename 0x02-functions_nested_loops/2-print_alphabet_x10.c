7d36449a22e093e584f33c59427fc499a404174f
Mustyatiq <atikumusti@gmail.com>
2023-01-11 01:47:07 -0800

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,14 @@
 
 /**
  * print_alphabet_x10 - prints the alphabet in lowercase 10 times
- * Return - void
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
 	int i, n;
 
-	for (i = 0 ;i <= 10; i++)
+	for (i = 0 ;i < 10; i++)
 	{
 		for (n = 0;n <= 25; n++)
 		{
