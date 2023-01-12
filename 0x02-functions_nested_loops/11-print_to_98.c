e5f0e170e7688c5596ac1ffb2d9e3eb444059824
Mustyatiq <atikumusti@gmail.com>
2023-01-12 01:09:12 -0800

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,42 +10,28 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; i++)
+	for (i = n; i < 100; i++)
 	{
-		if (n >= 0)
+		if (n < 0)
 		{
-			if (n != 98 && n > 9)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(44);
-				_putchar(32);
-			} else if (n != 98 && n < 10)
-			{
-				_putchar(n + '0');
-				_putchar(44);
-				_putchar(32);
-			} else
-				_putchar(n + '0');
-		} else
-		{
-			int m = n * -1;
-
 			_putchar('-');
-			if (n != 98 && n < -9)
-			{
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
-				_putchar(44);
-				_putchar(32);
-			} else if (n != 98 && n > -10)
-			{
-				_putchar(m + '0');
-				_putchar(44);
-				_putchar(32);
-			} else
-				_putchar(n + '0');
+			n *= -1;
 		}
+
+		if (n != 98 && n > 9)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(44);
+			_putchar(32);
+		} else if (n != 98 && n < 10)
+		{
+			_putchar(n + '0');
+			_putchar(44);
+			_putchar(32);
+		} else
+			_putchar(n + '0');
+	}
 		n = i;
 	}
 
